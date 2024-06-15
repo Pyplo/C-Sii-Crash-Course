331f4b9579540b7c9d58c7172f8d2e1bc3d1d09e
Patryk <Patrykp295@gmail.com>
2024-06-15 11:40:02 +0200

    Dodaj pliki projektów.

---
+++ PiewrszeZadanie/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PiewrszeZadanie
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            // To jest komentarz
+            Console.Write("Czesc Patryk");
+            Console.WriteLine("Witaj Patryku");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(2 + 2);
+            Console.WriteLine("2 + 2");
+
+            string imie = "Patryk";
+            string nazwisko = "Pytel";
+
+            Console.WriteLine("Witaj " + imie);
+
+        }
+    }
+}
